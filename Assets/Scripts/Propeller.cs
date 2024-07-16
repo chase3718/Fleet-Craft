7e02728067e404d7f784d7b65819725a894f1b67
IshiMoss <k.mitsu.1203@gmail.com>
2024-07-17 01:02:02 +0800

    Added engine spooling

--- Assets/Scripts/Propeller.cs
+++ Assets/Scripts/Propeller.cs
@@ -7,18 +7,27 @@ public class Propeller : MonoBehaviour, ShipMechanism
     public Ship ship {get; set;}
     public Rigidbody shipRb {get; set;}
     public ShipPart part {get; set;}
-    public Vector3 forceVector => part.transform.forward * ship.enginePower * part.propellerSpin;
+    public Vector3 fullForce => part.transform.forward * ship.enginePower * part.propellerSpin;
+    public Vector3 force = Vector3.zero;
+    public float throttle = 0.0f;
 
-    void Start()
-    {
-        
+    public void FixedUpdate(){
+        if( Input.GetKey( "w" )){ 
+            throttle = Mathf.MoveTowards(throttle, 1.0f, 0.01f);
+            Debug.Log( "Forward : "+ throttle );
+        } else if( Input.GetKey( "s" )){
+            throttle = Mathf.MoveTowards(throttle, -1.0f, 0.01f);
+            Debug.Log( "Backward : "+ throttle );
+        } else if( throttle != 0.0f ){ //spool to zero
+            throttle = Mathf.MoveTowards( throttle, 0.0f, 0.01f ); 
+            Debug.Log( "Zeroing : "+ throttle );
+        }
     }
 
     public void Update(){
-        if( Input.GetKey( "w" ) ){
-            shipRb.AddForce( forceVector );
-        } else if( Input.GetKey( "s" ) ){
-            shipRb.AddForce( -forceVector );
+        if( throttle != 0.0f ){
+            force = Vector3.LerpUnclamped( Vector3.zero, fullForce, throttle );
+            shipRb.AddForce( force );
         }
     }
 }
