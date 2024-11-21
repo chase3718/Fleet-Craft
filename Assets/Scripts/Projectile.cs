78d4249a017e491171f9d3f9b5552a612f10c7fa
IshiMoss <k.mitsu.1203@gmail.com>
2024-11-21 22:48:10 +0800

    Fixed projectile spawning with wrong hit ray

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -12,19 +12,20 @@ public class Projectile : MonoBehaviour
     private Boolean splashed = false;
     private 
 
-    void Start(){
+    void Awake(){
         projectileRB.detectCollisions = false;
         projectileRB.AddForce(initialVelocity,ForceMode.VelocityChange);
+        transform.localRotation = Quaternion.LookRotation(projectileRB.velocity.normalized);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         projectileRB.AddForce(Physics.gravity);
-        this.gameObject.transform.localRotation = Quaternion.LookRotation(projectileRB.velocity.normalized);
+        transform.localRotation = Quaternion.LookRotation(projectileRB.velocity.normalized);
 
         //Altitude Based Checks
-        if(this.gameObject.transform.position.y < 0.0 && !splashed){ //creates a splash
+        if(transform.position.y < 0.0 && !splashed){ //creates a splash
             GameObject splash = Resources.Load<GameObject>("Particles/WaterSplash");
             Instantiate(splash,
                 transform.position, 
@@ -32,14 +33,15 @@ public class Projectile : MonoBehaviour
                 );
             splashed = true;
         }
-        if(this.gameObject.transform.position.y < -10.0){
+        if(transform.position.y < -10.0){
             Destroy(this.gameObject);
         }
 
         //Collision against ship.
         //Using a ray system as it's more reliable.
-        Ray hitRay = new Ray(transform.position,transform.forward);
+        Ray hitRay = new Ray(transform.position, projectileRB.velocity.normalized );
         RaycastHit[] hits = Physics.RaycastAll(hitRay, 1.0f);
+        Debug.DrawRay(transform.position,transform.forward,Color.blue,5.0f);
         foreach(RaycastHit hit in hits){
             Floater f = hit.collider.GetComponentInParent<Floater>();
             if(f!= null){
